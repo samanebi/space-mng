2bf6c3f877ed0658a36c594e8b4102a9436b50cf
saman ebrahimpour <Ebrahimpoorsalimi.saman@gmail.com>
2022-07-15 13:14:12 +0430

    fix : controller advisor
    it does not match on corresponding exceptions.

--- src/main/java/com/park/spacemng/api/CustomizedExceptionControllerAdviser.java
+++ src/main/java/com/park/spacemng/api/CustomizedExceptionControllerAdviser.java
@@ -1,16 +1,12 @@
 package com.park.spacemng.api;
 
-import javax.validation.constraints.NotNull;
-
 import com.park.spacemng.exception.GeneralException;
 import com.park.spacemng.model.constants.ProcessStatus;
 import com.park.spacemng.model.response.GeneralResponse;
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -20,15 +16,6 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class CustomizedExceptionControllerAdviser extends ResponseEntityExceptionHandler {
 
-	@Override
-	@NotNull
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		log.error("method argument validation error : {}", ex.getMessage());
-		return new ResponseEntity<>(new GeneralResponse(ProcessStatus.FAILURE, ex.getMessage()),
-				HttpStatus.UNPROCESSABLE_ENTITY);
-	}
-
 	@ExceptionHandler(GeneralException.class)
 	public final ResponseEntity<GeneralResponse> handleGeneralException(GeneralException ex, WebRequest request) {
 		log.error("general exception happened : {}", ex.getMessage());
@@ -37,7 +24,7 @@ public class CustomizedExceptionControllerAdviser extends ResponseEntityExceptio
 	}
 
 	@ExceptionHandler(Exception.class)
-	public final ResponseEntity<GeneralResponse> handleException(GeneralException ex, WebRequest request) {
+	public final ResponseEntity<GeneralResponse> handleRegularException(Exception ex, WebRequest request) {
 		log.error("exception happened : {}", ex.getMessage());
 		return new ResponseEntity<>(new GeneralResponse(ProcessStatus.FAILURE, ex.getMessage()),
 				HttpStatus.UNPROCESSABLE_ENTITY);
